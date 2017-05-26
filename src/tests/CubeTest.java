23ce2813d91c6aececf2802a96d76bef52bcd456
somename <someemail>
2017-05-26 12:17:48 +0200

    besouro automatic message

--- src/tests/CubeTest.java
+++ src/tests/CubeTest.java
@@ -1,5 +1,16 @@
 package tests;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class CubeTest {
+	
+	@Test
+	public void notNull(){
+		
+		Cube cube = new Cube();
+		assertNotNull(cube);
+	}
 
 }
