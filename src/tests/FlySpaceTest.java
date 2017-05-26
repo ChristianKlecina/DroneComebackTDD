e907e169ab82cfde7a96cb1098e565e5302a0dd1
somename <someemail>
2017-05-26 12:32:11 +0200

    besouro automatic message

--- src/tests/FlySpaceTest.java
+++ src/tests/FlySpaceTest.java
@@ -1,5 +1,19 @@
 package tests;
 
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import drone.Cube;
+
 public class FlySpaceTest {
+	
+	@Test
+	public void nullTest(){
+		FlySpace fly = new FlySpace();
+		assertNotNull(fly);
+	}
+	
+	
 
 }
