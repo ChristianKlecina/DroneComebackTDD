ca31695b3192785baf074da39c562c966065af25
somename <someemail>
2017-05-26 12:47:30 +0200

    besouro automatic message

--- src/drone/FlySpace.java
+++ src/drone/FlySpace.java
@@ -2,6 +2,12 @@ package drone;
 
 public class FlySpace {
 	
-	private int[] boundariesVeca = {}
-
+	private int[] boundariesVecaPocetna = {0,0,0};
+	private int[] boundariesVecaKrajnja = {50,50,50};
+	private int[] boundariesManjaPocetna = {10,10,10};
+	private int[] boundariesManjaKrajnja = {40,40,40};
+	
+	FlySpace space = new FlySpace(boundariesVecaPocetna, boundariesVecaKrajnja, boundariesManjaPocetna, boundariesManjaKrajnja){
+		
+	}
 }
