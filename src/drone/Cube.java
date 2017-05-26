a05a49e54b0317c8949c253fb7c1b32a4c8040ad
somename <someemail>
2017-05-26 12:21:24 +0200

    besouro automatic message

--- src/drone/Cube.java
+++ src/drone/Cube.java
@@ -5,6 +5,9 @@ public class Cube {
 	private int[] boundariesEnd = {50, 50,50 };
 	private int[] boundariesStart = {0, 0,0 };
 	
+	private int[] boundariesEnd = {40, 40,40 };
+	private int[] boundariesStart = {10, 10,10 };
+	
 	public Cube(){
 		
 	}
