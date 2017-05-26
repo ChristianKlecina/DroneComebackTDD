ce46f0a313c038be2e152cc4fa4a16899f488f54
somename <someemail>
2017-05-26 12:39:41 +0200

    besouro automatic message

--- src/tests/CubeTest.java
+++ src/tests/CubeTest.java
@@ -11,7 +11,10 @@ public class CubeTest {
 	@Test
 	public void notNull(){
 		
-		Cube cube = new Cube({0,0,0},{50,50,50});
+		int[] pocetna = {0,0,0};
+		int[] krajnja = {50,50,50};
+		
+		Cube cube = new Cube (int[] pocetna,int[] krajnja);
 		assertNotNull(cube);
 	}
 
